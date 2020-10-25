a46c3492f6e8ca4e5a7f22cbad030434d05d9cc7
MathieuBesson <mathieubesson01@orange.fr>
2020-10-25 23:55:10 +0100

    --- Continue graphic integration

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = () => { 
+
+    var app = document.getElementById('text-typing');
+    
+    var typewriter = new Typewriter(app, {
+      loop: true,
+      delay: 150,
+    });
+    
+    typewriter
+      .pauseFor(1000)
+      .typeString('Développeur Web <i class="em em-rocket"></i>')
+      .pauseFor(5000)
+      .deleteChars(16)
+      .typeString('À votre service ! <i class="em em-spock-hand"></i>')
+      .pauseFor(5000)
+      .start();
+
+
+
+    let menuList = document.querySelectorAll('.menu-list-item');
+      
+    menuList.forEach(item => {
+        item.addEventListener('click', () => {
+			menuList.forEach(element => {
+				element.classList.remove('active');
+			});
+			item.classList.add('active')
+        });
+    });
+    console.log(menuList);
+}
+
